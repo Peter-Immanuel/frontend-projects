5fbd8bbe40810fc696952e52b73637841656f755
Peter-Immanuel <peterbemshima@gmail.com>
2023-05-26 13:27:02 +0100

    Lesson: v-for

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -12,8 +12,11 @@ const app = Vue.createApp({
         return {
             product: "Socks",
             image: "./src/assets/images/socks_green.jpg",
-            onSale: false,
-            productLink : "https://images.unsplash.com/photo-1509281373149-e957c6296406?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
+            details: ["50% cotton, '30% wool, '20% polyester"],
+            variant: [
+                {id: 2234, color: "green"},
+                {id: 2235, color: "blue"},
+            ]
         }
     }
 })
