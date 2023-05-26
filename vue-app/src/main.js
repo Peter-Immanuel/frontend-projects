ee527daf37f655e5103c5d490f6ffde03ce556f5
Peter-Immanuel <peterbemshima@gmail.com>
2023-05-26 14:56:30 +0100

    Lesson challenge: create a loop for sizes

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -10,13 +10,16 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: "Socks",
             image: "./src/assets/images/socks_green.jpg",
-            details: ["50% cotton, '30% wool, '20% polyester"],
-            variant: [
+            details: ["50% cotton", "30% wool", "20% polyester"],
+            variants: [
                 {id: 2234, color: "green"},
                 {id: 2235, color: "blue"},
-            ]
+            ],
+            sizes: [3, 4, 6, 8, 9],
+            // sizes: []
         }
     }
 })
