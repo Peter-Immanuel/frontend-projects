3f69cefb28421cd2550559707884b645cf18b88d
Peter-Immanuel <peterbemshima@gmail.com>
2023-06-02 05:53:46 +0100

    Idan do build react app 😂

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
-import React from "react";
+import { useState, useEffect} from "react";
 
+import './App.css'
+import searchIcon from './search.svg';
+
+// componet imports
+import MovieCard from './MovieCard';
+
+
+
+const API_URL = "https://omdbapi.com?apikey=93845995"
 
 const App = () => {
+
+    // state 
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+    
+    useEffect(() => {
+        searchMovies("superman")
+    }, []);
+    
     return (
-        <h1>My Reacts App</h1>
+        <div className="app">
+            <h1>MovieLand for IDANS only</h1>
+
+            {/* Search bar component */}
+            <div className="search">
+                <input 
+                    placeholder="Idan type your search 🤲🏽"
+                    value={searchTerm}
+                    onChange={(e) => {
+                        setSearchTerm(e.target.value)
+                    }}
+                />
+
+                <img 
+                    src={searchIcon}
+                    alt="search"
+                    onClick={() => {
+                        searchMovies(searchTerm)
+                    }}
+                />
+            </div>
+            {movies.length > 0 
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => <MovieCard movie={movie} />)}
+                    </div>
+                ) : (
+                    <div className="empty"> 
+                        <h2> Idan no vex we still the cook 🧑🏽‍🍳</h2>
+                    </div>
+                )
+            }            
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import  ReactDOM  from 'react-dom'
 
-const API_URL = "https://omdbapi.com?apikey=93845995"
 
 import App from './App'
 
